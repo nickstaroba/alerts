599adc620faecdc70664a8861c28ea08e660332e
Nick Staroba <nick@nickstaroba.com>
2022-09-23 20:20:14 -0400

    adding hash routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import "./styles.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
+import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
 
 import { AlertsProvider } from "./AlertsProvider";
 import App from "./App";
 import theme from "./theme";
+
+const currentLocation = () => window.location.hash.replace(/^#/, "") || "/";
+
+const navigate = (to) => {
+  window.location.hash = to;
+};
+
+const useHashLocation = () => {
+  const [loc, setLoc] = useState(currentLocation());
+
+  useEffect(() => {
+    const handleHashchange = () => setLoc(currentLocation());
+
+    window.addEventListener("hashchange", handleHashchange);
+    return () => window.removeEventListener("hashchange", handleHashchange);
+  }, []);
+
+  return [loc, navigate];
+};
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -16,7 +37,9 @@ root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <AlertsProvider>
-      <App />
+      <Router hook={useHashLocation}>
+        <App />
+      </Router>
     </AlertsProvider>
   </ThemeProvider>
 );
